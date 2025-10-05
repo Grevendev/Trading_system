d8ddcc171a65c4dbeea1f37312b3ed728ac069d9
Grevendev <edvin.lindborg@hotmail.se>
2025-10-05 12:33:16 +0200

    Uppdaterat kommentarer.

--- IUser.cs
+++ IUser.cs
@@ -3,17 +3,18 @@ using System;
 namespace Trading_System
 {
   /// <summary>
-  /// Interface som definierar gemensamma metoder för alla användare.
+  /// Interface som definierar gemensamma egenskaper och metoder
+  /// för alla typer av användare (Admin, Trader).
   /// </summary>
   public interface IUser
   {
-    string GetUsername();
-    string GetPassword();
-    string GetName();
-    string GetRole();
-    bool GetIsActive();
-    void SetIsActive(bool active);
-    bool TryLogin(string username, string password);
-    void Info();
+    string GetUsername(); //Retunerar användarens unika användarnamn.
+    string GetPassword(); // Retunerar användarens lösenord (i klartext i detta fall).
+    string GetName(); // Retunerar användarens fullständiga namn.
+    string GetRole(); // Retunerar användarens till, t.ex. "Admin" eller "Trader".
+    bool GetIsActive(); //Anger om användaren är aktiv (t.ex. inte avtängd).
+    void SetIsActive(bool active); // Sätter användarens aktiva status.
+    bool TryLogin(string username, string password); // Försöker logga in användaren genom att jämföra användarnamn och lösenord.
+    void Info(); // Skriver ut användarens information till konsolen. 
   }
 }
