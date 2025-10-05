d820e88477a20e7ed36c36b94636c00dcd35332a
Grevendev <edvin.lindborg@hotmail.se>
2025-10-05 12:20:37 +0200

    Uppdaterat kommentarer.

--- Item.cs
+++ Item.cs
@@ -3,14 +3,15 @@ using System;
 namespace Trading_System
 {
   /// <summary>
-  /// Representerar ett item som kan bytas mellan Traders.
+  /// Representerar ett item som ägs av en Trader och kan bytas i en trade.
   /// </summary>
   public class Item
   {
-    private string _name;
-    private string _description;
-    private string _ownerUsername;
+    private string _name; // Namnet på föremålet.
+    private string _description; // En kort beskriving av föremålet.
+    private string _ownerUsername; // Användarnamnet på den som äger förmålet
 
+    // Skapar ett nytt item med namn, beskriving och ägare.
     public Item(string name, string description, string ownerUsername)
     {
       _name = name;
@@ -18,10 +19,19 @@ namespace Trading_System
       _ownerUsername = ownerUsername;
     }
 
+    // Getters
+    //Returnerar namnet på föremålet.
     public string GetName() => _name;
+
+    //Retunerar beskrivingen av förmålet.
     public string GetDescription() => _description;
+
+    //Retunerar användarnamnet på ägaren.
     public string GetOwnerUsername() => _ownerUsername;
 
+
+    //Funktionalitet
+    //Byter ägare på föremålet till en ny användare.
     public void ChangeOwner(string newOwner)
     {
       _ownerUsername = newOwner;
